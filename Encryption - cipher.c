d7d0690cbc4becc6120f14c83dee4808a41fa7ec
Michael Maurice <michaelmaurice92@gmail.com>
2019-04-08 09:16:44 +0000

    wrote code to take text from file and print each character

--- Encryption - cipher.c
+++ Encryption - cipher.c
@@ -2,5 +2,33 @@
 
 int main(void)
 {
+    char c;
+    int asc[1024];
+    int i;
     
+    //Open message file
+    
+    FILE *input; 
+    
+    input = fopen("Task 1 message", "r");
+    if(input == NULL) {
+        perror("fopen()");
+    return 0;
+    }
+    
+       
+    for(i = 0; i <= 15; i++){
+     
+        fscanf(input,"%c", &c);      
+        asc[i] = c;
+        printf("%c\n", asc[i]);
+    }    
+
+   
+
+
+  
+   return 0; 
+
 }
+
