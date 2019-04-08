78d0cd58bb7423e06a3ff469ee347e23d3e0a063
Michael Maurice <michaelmaurice92@gmail.com>
2019-04-08 22:52:16 +0000

    confines printed text to ASCII letters and converts non-cap to capital

--- Encryption - cipher.c
+++ Encryption - cipher.c
@@ -5,11 +5,10 @@ int main(void)
     char c, e;
     int asc[1024];
     int i;
-    int k = 5;
+    int k = 5;  //key
     
     //Open message file
     FILE *input;        //File points to input
-    
     input = fopen("Task 1 message", "r");   //open file name and read format
     if(input == NULL) {         //Error message incase file directory not found
         perror("fopen()");
@@ -22,8 +21,21 @@ int main(void)
         
         fscanf(input,"%c", &c);      
         asc[i] = c;
-        e = (c+k);  //encryption algorithm 
-        printf("%c %d %c\n", asc[i], asc[i], e); //prints array character & ASCII value, then encrypted array character
+        e = (c+k);  //encryption algorithm  
+        /*prints array character & ASCII value, then encrypted array character*/
+        
+        if(e >= 65 && e <= 90){
+            printf("%c %d %c\n", asc[i], asc[i], e);    //prints capital letters
+        }
+        else if(e >= 97 && e <= 122){                   //converts non-capital to capital and prints
+            printf("%c %d %c\n", asc[i]-32, asc[i]-32, e-32); 
+        }
+    
+
+
+
+        
+
         
         
 
