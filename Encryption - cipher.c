b2c4ef9e58030cd554892f79ed761fe517b4fa72
Michael Maurice <michaelmaurice92@gmail.com>
2019-04-09 01:09:42 +0000

    added an array of capital letters for encrypted text

--- Encryption - cipher.c
+++ Encryption - cipher.c
@@ -2,9 +2,10 @@
 
 int main(void)
 {
-    char c, e;  
+    char c, eN;
+    int eLib[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
     int asc[1024];
-    int i;
+    int i , e;
     int k = 5;  //key
     
     //Open message file
@@ -22,20 +23,24 @@ int main(void)
         fscanf(input,"%c", &c); 
         if(c >= 65 && c <= 90){ // upper case
             asc[i] = c;  //assigns array at array point i with value c
-            e = (c+k);  //encryption algorithm  
+            c = c - 65; //A = 0
+            e = c + k; //encryption algorithm 
+            eN = eLib[e];  //selects encrypted value from array 
         }   
 
         else if(c >= 97 && c <=122){  //lower case
             asc[i] = c - 32;  //
-            e = (c+k) - 32;  //
+            c = c - 97; 
+            e = c + k;
+            eN = eLib[e];           
         }
         
         else if(c < 65 || (c >= 91 && c <= 96) || c > 122 ){ //excludes any ASCII value that isn't a letter
             asc[i] = 32;  //
-            e = 32;  //
+            eN = 32;  //
         }   
 
-        printf("%c %d %c\n", asc[i], asc[i], e);
+        printf("%c %d %c %d\n", asc[i], asc[i], eN, eN);
     }
   
    return 0; 
